42d6ef7dca8840bd14dc4fdea51aae7efe3a2715
skep <skep@rama>
2024-04-04 21:11:35 +0000

    chore: add Debug build type (simultaneously)

--- build-flamegpu.sh
+++ build-flamegpu.sh
@@ -8,18 +8,20 @@ python3 -m pip install -r requirements.txt
 
 git submodule update --init --recursive
 
-[[ $CLEAN == true ]] && rm -rf third_party/FLAMEGPU2/build
-mkdir -p third_party/FLAMEGPU2/build
-cd third_party/FLAMEGPU2/build
+build_type=Release
+[[ $CLEAN == true ]] && rm -rf third_party/FLAMEGPU2/build-$build_type
+mkdir -p third_party/FLAMEGPU2/build-$build_type
+cd third_party/FLAMEGPU2/build-$build_type
 cmake_opts=(
   -G Ninja
-  -DCMAKE_BUILD_TYPE=Release
-  -DFLAMEGPU_VISUALISATION=ON
+  -DCMAKE_BUILD_TYPE=$build_type
+  -DFLAMEGPU_VISUALISATION=OFF
   -DFLAMEGPU_BUILD_PYTHON=ON
 )
 cmake .. ${cmake_opts[@]}
-ninja
+ninja -t compdb > compilation_database.json
+ninja pyflamegpu
 python3 -m venv venv
 source ./venv/bin/activate
-python3 -m pip install ./lib/Release/python/dist/pyflamegpu-2*-linux_x86_64.whl
+python3 -m pip install ./lib/$build_type/python/dist/pyflamegpu-2*-linux_x86_64.whl
 python3 -m pip install ipython
